55637a868546efc5b162be0b1e84073ef650b04a
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-10 01:18:51 +0300

    Update styled files
    Update styled files

--- src/components/Feedback/Feedback.styled.js
+++ src/components/Feedback/Feedback.styled.js
@@ -1,8 +1,22 @@
 import styled from '@emotion/styled';
 
 export const Options = styled.button`
-  margin: 5px;
-  :hover {
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.25;
+
+  margin: 12px 8px;
+  text-transform: uppercase;
+
+  background-color: antiquewhite;
+  border-radius: 4px;
+  width: 80px;
+
+  :hover,
+  :focus {
     background-color: orange;
+    border-radius: 4px;
+    border-color: orange;
+    color: white;
   }
 `;
--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,11 +1,14 @@
 import styled from '@emotion/styled';
 
 export const SectionBox = styled.div`
+  text-align: center;
+
   margin: 10px;
   padding: 5px;
   width: 320px;
-  background-color: yellow;
-  border: 1px solid black;
+
+  background-color: beige;
+  border: 1px solid #cec074;
+  box-shadow: 0.1em 0.1em 0.3em rgba(0, 0, 0, 0.2);
   border-radius: 5px;
-  text-align: center;
 `;
